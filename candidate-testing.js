f103c67910fa7d8ce7ecc74b95a4b00915d6a7d2
Dion Duncan <84429546+djion1820@users.noreply.github.com>
2021-07-31 02:59:38 +0000

    I want this to run a test

--- candidate-testing.js
+++ candidate-testing.js
@@ -77,6 +77,37 @@ let grade = rightAnswers/numberOfq * 100;
 console.log ("Over all grade", grade);
 console.log("You pass if you get a 'true'(pass) and 'false' (failed) if you get a false. This test is...", 80<= grade);
 
+//function askForName(candidateName) {
+  // TODO 1.1b: Ask for candidate's name //
+
+
+
+//function askQuestion(candidateAnswer) {
+  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+
+
+
+
+function gradeQuiz(candidateAnswers) {}
+
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+
+
+  
+  
+
+ // return grade;
+
+
+//function runProgram() {
+  //askForName();
+  // TODO 1.1c: Ask for candidate's name //
+  
+ // askQuestion();
+  //gradeQuiz(this.candidateAnswers);
+
+
+
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
 module.exports = {
